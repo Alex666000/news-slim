55246046db9426c5c45385c471902b9e5f7fcd16
Alex666000 <bogdanov777000@mail.ru>
2023-10-09 15:54:45 +0300

    add Pagination for news

--- src/api/apiNew.js
+++ src/api/apiNew.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
 
-export const getNews = async () => {
+const BASE_URL = 'https://api.currentsapi.services/v1/'
+const API_KEY = 'HvM0Wi9uuxXGeAeNvqKFBxRASYlBUmqcnGeZfNvF66XRrbd-'
+
+export const getNews = async (page_number = 1, page_size = 10 ) => {
     try {
-        const response = await axios.get('https://api.currentsapi.services/v1/latest-news', {
+        const response = await axios.get( `${BASE_URL}search`, {
             params: {
-                apiKey: 'HvM0Wi9uuxXGeAeNvqKFBxRASYlBUmqcnGeZfNvF66XRrbd-'
+                apiKey: API_KEY,
+                page_number,
+                page_size,
             }
         })
         return response.data // Получили данные
@@ -13,4 +18,7 @@ export const getNews = async () => {
         console.log(error)
     }
 }
+/*
+Принимаем одну страницу и 10 элементов на ней
+ */
 
